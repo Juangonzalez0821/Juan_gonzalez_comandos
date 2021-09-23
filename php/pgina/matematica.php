42fe768ae91956b94a8a22392e1ebc75c4c11058
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2021-09-22 23:20:35 -0500

    Create matematica.php

---
+++ php/pgina/matematica.php
@@ -0,0 +1,24 @@
+<?php
+class matematica {
+    public static function sumar($num1,$num2){
+        $suma=$num1+$num2;
+        return $suma;
+    }
+// esto realiza una suma de variables de num1 + num 2
+
+    public static function restar($num1,$num2){
+        $resta=$num1-$num2;
+        return $resta;
+    }
+// esto realiza una resta de variables de num1 - num 2
+    public static function multiplicar($num1,$num2){
+        $multi=$num1*$num2;
+        return $multi;
+    }
+// esto realiza una multi de variables de num1 * num2
+    public static function dividir($num1,$num2){
+        $divi=$num1/$num2;
+        return $divi;
+    }
+}// esto realiza una divi de variables de num1 / num2
+?>
